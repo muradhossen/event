ce01a53bb0470f06e8cadca0999a9da3b3391e19
muradhossen <muradbabu006@gmail.com>
2025-01-09 13:01:57 +0600

    update start up.cs

--- API/Startup.cs
+++ API/Startup.cs
@@ -121,8 +121,7 @@ namespace API
                     option.TokenValidationParameters = new TokenValidationParameters
                     {
                         ValidateIssuerSigningKey = true,
-                        //IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"])),
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("U3VwZXIgc2VjcmV0IHVuZ2Vzc2FibGUgdG9rZW4=U3VwZXIgc2VjcmV0IHVuZ2Vzc2FibGUgdG9rZW4=")),
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"])),
                         ValidateIssuer = false,
                         ValidateAudience = false
                     };
